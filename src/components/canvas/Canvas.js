091e7d52aa6349a49694c99c3cec678a7c5a3032
Alpha-Lucky <alpha.lucky.m@gmail.com>
2021-10-30 20:11:35 +0300

    added datas for canvasData

--- src/components/canvas/Canvas.js
+++ src/components/canvas/Canvas.js
@@ -4,6 +4,7 @@ import logo from '../img/logok.jpg';
 import GenresContainer from './painting/genres/GenresContainer';
 
 import PreloaderOut from '../common/Preloader/Preloader';
+import ScrollToTop from '../../ScrollToTop/ScrollToTop';
 
 
 
@@ -18,6 +19,7 @@ const Paintings = (props) => {
 
     return (
         <div className="canvas__container_index">
+            <ScrollToTop />
                 <PreloaderOut preloaderStatus={loading} />
             <div className="container__canvas_content">
                 <div className="logoCanvas">
