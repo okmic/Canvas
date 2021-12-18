0b554ffeb563ef21ba4415efc62b6ac0ff671169
Michael <alpha.lucky.m@gmail.com>
2021-12-18 15:09:53 +0300

    preloading.tsx

--- src/components/common/Preloader/PreloaderContainer.tsx
+++ src/components/common/Preloader/PreloaderContainer.tsx
@@ -2,12 +2,8 @@ import React from "react";
 import './preloader.css'
 import ReactLoading from 'react-loading';
 
-let PreloaderContainerHesh: React.FC = () =>  {
-    return(
-        <div className="preloader_cotnainer_hesh">
+let PreloaderContainerHesh: React.FC = () => <div className="preloader_cotnainer_hesh">
              <ReactLoading type={"bubbles"} color={'#fff'} height={70} width={300} />
         </div>
-    )
-}
 
 export default PreloaderContainerHesh
