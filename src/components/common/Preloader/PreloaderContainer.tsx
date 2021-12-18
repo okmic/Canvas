34b8a169e21e1940e84ae061398a68f3bf5f8ea8
Michael <alpha.lucky.m@gmail.com>
2021-12-18 15:05:05 +0300

    preloading.tsx

--- src/components/common/Preloader/PreloaderContainer.tsx
+++ src/components/common/Preloader/PreloaderContainer.tsx
@@ -2,9 +2,7 @@ import React from "react";
 import './preloader.css'
 import ReactLoading from 'react-loading';
 
-let PreloaderContainerHesh = (props) =>  {
-
-
+let PreloaderContainerHesh: React.FC = () =>  {
     return(
         <div className="preloader_cotnainer_hesh">
              <ReactLoading type={"bubbles"} color={'#fff'} height={70} width={300} />
--- src/components/common/Preloader/inPreloader.tsx
+++ src/components/common/Preloader/inPreloader.tsx
@@ -2,14 +2,12 @@ import React from "react";
 import './preloader.css'
 import ReactLoading from 'react-loading';
 
-let PreloaderIn = (props) =>  {
+type PropsType = {
+    preloaderStatus: boolean
+}
 
-
-    return(
-        <div className={props.preloaderStatus ? "preloader" : "preloader active"}>
+let PreloaderIn: React.FC<PropsType> = (props) =>  <div className={props.preloaderStatus ? "preloader" : "preloader active"}>
              <ReactLoading type={"bars"} color={'#964b00'} height={30} width={100} />
         </div>
-    )
-}
 
 export default PreloaderIn
